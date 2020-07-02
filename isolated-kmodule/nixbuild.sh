27a7fdacbce4848b84183f691cc8934e15e54ca5
Can Selcik <selcik.can+vmgh@gmail.com>
2020-07-01 17:21:59 -0700

    isolated kmodule

---
+++ isolated-kmodule/nixbuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+KROOT=$(nix-build -E '(import <nixpkgs> {}).linuxPackages_latest.kernel.dev' --no-out-link)
+KDIR="$KROOT/lib/modules/*/build"
+
+if [ ! -d $KDIR ]; then
+	echo "Kernel Build Directory is invalid: $KDIR"
+	exit 1
+fi
+
+KDIR=${KDIR} make
